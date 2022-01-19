5f5bc121726374bc3407fe082d8c2b3073bdc179
glebbash <glebbash@gmail.com>
2022-01-19 16:56:51 +0000

    feat(enum)!: use named enums by default

--- src/decorators/is-enum.spec.ts
+++ src/decorators/is-enum.spec.ts
@@ -1,14 +1,32 @@
 import { Result } from 'true-myth';
 
-import { input, make } from '../../tests/helpers';
+import { generateSchemas, input, make } from '../../tests/helpers';
 import { IsEnum } from '../nestjs-swagger-dto';
 
 describe('IsEnum', () => {
   describe('array enum', () => {
     class Test {
-      @IsEnum({ enum: [1, 2] })
+      @IsEnum({ enum: { name: 'OneOrTwo', values: [1, 2] } })
       enumField!: 1 | 2;
     }
+
+    it('generates correct schema', async () => {
+      expect(await generateSchemas([Test])).toStrictEqual({
+        OneOrTwo: {
+          enum: [1, 2],
+          type: 'string',
+        },
+        Test: {
+          type: 'object',
+          properties: {
+            enumField: {
+              $ref: '#/components/schemas/OneOrTwo',
+            },
+          },
+          required: ['enumField'],
+        },
+      });
+    });
 
     it('accepts specified enum', async () => {
       expect(await input(Test, { enumField: 1 })).toStrictEqual(
@@ -45,9 +63,27 @@ describe('IsEnum', () => {
     }
 
     class Test {
-      @IsEnum({ enum: Enum })
+      @IsEnum({ enum: () => Enum })
       enumField!: Enum;
     }
+
+    it('generates correct schema', async () => {
+      expect(await generateSchemas([Test])).toStrictEqual({
+        Enum: {
+          enum: ['On', 'Off'],
+          type: 'string',
+        },
+        Test: {
+          type: 'object',
+          properties: {
+            enumField: {
+              $ref: '#/components/schemas/Enum',
+            },
+          },
+          required: ['enumField'],
+        },
+      });
+    });
 
     it('accepts specified enum', async () => {
       expect(await input(Test, { enumField: 'On' })).toStrictEqual(
@@ -71,17 +107,89 @@ describe('IsEnum', () => {
 
       for (const testValue of testValues) {
         expect(await input(Test, testValue)).toStrictEqual(
-          Result.err('enumField must be a valid enum value')
+          Result.err('enumField must be one of the following values: On, Off')
         );
       }
     });
   });
 
-  describe('array', () => {
+  describe('raw enum', () => {
     class Test {
-      @IsEnum({ enum: ['a', 'b'], isArray: true })
+      @IsEnum({ enum: { raw: [1, 2] } })
+      enumField!: 1 | 2;
+    }
+
+    it('generates correct schema', async () => {
+      expect(await generateSchemas([Test])).toStrictEqual({
+        Test: {
+          type: 'object',
+          properties: {
+            enumField: {
+              enum: [1, 2],
+              type: 'number',
+            },
+          },
+          required: ['enumField'],
+        },
+      });
+    });
+
+    it('accepts specified enum', async () => {
+      expect(await input(Test, { enumField: 1 })).toStrictEqual(
+        Result.ok(make(Test, { enumField: 1 }))
+      );
+      expect(await input(Test, { enumField: 2 })).toStrictEqual(
+        Result.ok(make(Test, { enumField: 2 }))
+      );
+    });
+
+    it('rejects everything else', async () => {
+      const testValues: unknown[] = [
+        { enumField: 'true' },
+        { enumField: 'false' },
+        { enumField: 0 },
+        { enumField: [] },
+        { enumField: {} },
+        { enumField: null },
+        {},
+      ];
+
+      for (const testValue of testValues) {
+        expect(await input(Test, testValue)).toStrictEqual(
+          Result.err('enumField must be one of the following values: 1, 2')
+        );
+      }
+    });
+  });
+
+  describe('array of enums', () => {
+    const AOrB = ['a', 'b'];
+
+    class Test {
+      @IsEnum({ enum: () => AOrB, isArray: true })
       enumField!: ('a' | 'b')[];
     }
+
+    it('generates correct schema', async () => {
+      expect(await generateSchemas([Test])).toStrictEqual({
+        AOrB: {
+          enum: ['a', 'b'],
+          type: 'string',
+        },
+        Test: {
+          type: 'object',
+          properties: {
+            enumField: {
+              type: 'array',
+              items: {
+                $ref: '#/components/schemas/AOrB',
+              },
+            },
+          },
+          required: ['enumField'],
+        },
+      });
+    });
 
     it('accepts enum arrays', async () => {
       expect(await input(Test, { enumField: ['a', 'b'] })).toStrictEqual(
--- src/decorators/is-enum.ts
+++ src/decorators/is-enum.ts
@@ -1,13 +1,68 @@
-import { IsEnum as IsEnumCV, IsIn } from 'class-validator';
+import { IsIn } from 'class-validator';
 
 import { compose, PropertyOptions } from '../core';
 
-export const IsEnum = <T>({
-  enum: e,
+export type EnumValue<T extends number | string> = T[] | Record<string, T>;
+export type EnumOptions<T extends number | string> =
+  | { raw: EnumValue<T> }
+  | { name: string; values: EnumValue<T> }
+  | (() => EnumValue<T>);
+
+/**
+ * Usage:
+ * ```ts
+ * enum OneOrTwo { One = 1, Two = 2, }
+ * IsEnum({ enum: () => OneOrTwo })
+ *
+ * const OneOrTwo = [1, 2];
+ * IsEnum({ enum: () => OneOrTwo })
+ *
+ * IsEnum({ enum: { name: 'OneOrTwo', values: [1, 2] } })
+ *
+ * IsEnum({ enum: { raw: [1, 2] } }) // Will not create a new type
+ * ```
+ *
+ * Note: Enum name is extracted from the string representation
+ * of lambda function excluding `() => ` part
+ */
+export const IsEnum = <T extends number | string>({
+  enum: enumOptions,
   ...base
-}: PropertyOptions<T, { enum: T[] | Record<string, T> }>): PropertyDecorator =>
-  compose(
-    { type: 'enum', enum: e },
+}: PropertyOptions<T, { enum: EnumOptions<T> }>): PropertyDecorator => {
+  const { enumValues, enumName } = getEnumNameAndValues(enumOptions);
+
+  const enumValuesArray = Object.values(enumValues);
+
+  return compose(
+    { type: typeof enumValuesArray[0], enum: enumValuesArray, enumName },
     base,
-    Array.isArray(e) ? IsIn(e, { each: !!base.isArray }) : IsEnumCV(e, { each: !!base.isArray })
+    IsIn(enumValuesArray, { each: !!base.isArray })
   );
+};
+
+function getEnumNameAndValues<T extends number | string>(
+  e: EnumOptions<T>
+): {
+  enumValues: T[] | Record<string, T>;
+  enumName?: string;
+} {
+  if ('raw' in e) {
+    return { enumValues: e.raw };
+  }
+
+  if (typeof e !== 'function') {
+    return { enumValues: e.values, enumName: e.name };
+  }
+
+  return { enumValues: e(), enumName: getEnumNameFromFunction(e) };
+}
+
+function getEnumNameFromFunction(e: () => unknown) {
+  const enumName = e.toString().split('=>')[1].trim();
+
+  if (!/^\w+$/.test(enumName)) {
+    throw new Error(`Invalid enum name: ${enumName}`);
+  }
+
+  return enumName;
+}
